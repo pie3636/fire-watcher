96753d94f34509e249232758d782735078d6876d
Maxime Méloux <maxime.meloux@ensimag.grenoble-inp.fr>
2015-05-24 20:45:48 +0200

    Balancing

--- data.js
+++ data.js
@@ -11,7 +11,7 @@ sparklingEmbers: {
     probability: 0.014
 */
     var game = {
-    version: "v0.7.6",
+    version: "v0.7.7",
     onLoad: false, // Restore purchases
     onImport: false, // Load from import
     onReset: false, // Load from initValues
